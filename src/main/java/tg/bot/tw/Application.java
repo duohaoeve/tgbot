1537f01f61c549321ec39a3575d600c89df06d4d
tenghao <tenghao@hundwe.com>
2025-03-14 11:07:20 +0800

    add:同步部分代码

--- src/main/java/tg/bot/tw/Application.java
+++ src/main/java/tg/bot/tw/Application.java
@@ -8,7 +8,32 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+//        String botToken = "8170478387:AAFzBnra_DGYvYAUkTA3seDF_gCFhvDDfko";
+//        //简单回复
+//        try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
+//            botsApplication.registerBot(botToken, new MyAmazingBot());
+//            System.out.println("MyAmazingBot successfully started!");
+//            Thread.currentThread().join();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
+        //照片
+//        try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
+//            botsApplication.registerBot(botToken, new PhotoBot(botToken));
+//            System.out.println("PhotoBot successfully started!");
+//            Thread.currentThread().join();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        //日志
+//        try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
+//            botsApplication.registerBot(botToken, new EmojiTestBot(botToken));
+//            System.out.println("LoggingTestBot successfully started!");
+//            Thread.currentThread().join();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 
 }
